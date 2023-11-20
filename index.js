4c6cc80d89a7e891622fa1bec26153fe1e1a4b12
nithinshankar123 <94787280+nithinshankar123@users.noreply.github.com>
2023-11-20 12:15:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,36 +11,30 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const commentRoute = require("./routes/comments");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("database is connected successfully!");
-  } catch (err) {
-    console.log(err);
-  }
-};
-//middlewares
-
+// Load environment variables
 dotenv.config();
+
+// Middleware
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(cookieParser());
 
+// CORS configuration
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+// Routes
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/comments", commentRoute);
 
-//image upload
-//image upload
+// Image upload
 const storage = multer.diskStorage({
   destination: (req, file, fn) => {
     fn(null, "images");
   },
   filename: (req, file, fn) => {
     fn(null, req.body.img);
-    // fn(null,"image1.jpg")
   },
 });
 const upload = multer({ storage: storage });
@@ -49,14 +43,28 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("Image has been uploaded successfully!");
 });
 
-app.listen(process.env.PORT, () => {
-  connectDB();
-  console.log("app is running on port 5000");
-});
+// Database connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database connected successfully!");
+    
+    // Start the server after the database connection is established
+    app.listen(process.env.PORT, () => {
+      console.log("App is running on port " + process.env.PORT);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-//static files
+// Static files
 app.use(express.static(path.join(__dirname, "./frontend/dist")));
 
+// Catch-all route
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./frontend/dist/index.html"));
 });
+
+// Call the connectDB function to initiate the application
+connectDB();
